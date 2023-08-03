4a8f66c622027d2afee1f206bc9e0e17cd025037
IrynaDidkivska <ididkivska@gmail.com>
2023-08-03 15:22:18 +0200

    init

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import 'modern-normalize/modern-normalize.css';
+import { Global } from 'components/styles/Global';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Global />
   </React.StrictMode>
 );
